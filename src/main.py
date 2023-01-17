d59d461062304dd097457aca18340d9011977202
Fernando de Oliveira <5161098+fedeoliv@users.noreply.github.com>
2023-01-17 01:24:47 -0300

    feature: kafka messages consumption on the Spark app

--- src/main.py
+++ src/main.py
@@ -3,15 +3,12 @@ from pyspark.sql import SparkSession
 # build spark session
 spark = SparkSession.builder.appName("SparkKafkaDemo").getOrCreate()
 
-df = spark.createDataFrame([
-    ('America/New_York', '2021-03-02 10:05:00'),
-    ('Europe/Lisbon', '2020-02-02 10:00:00'),
-    ('Europe/Madrid', '2021-02-17 10:00:00'),
-    ('Europe/London', '2021-06-02 12:00:00'),
-    ('America/Sao_Paulo', '2021-05-06 10:00:00'),
-    ('America/Sao_Paulo', '2024-07-11 10:00:00'),
-    ('America/Sao_Paulo', '2021-02-01 10:00:00'),
-    ('Africa/Nairoui', '2025-12-02 13:04:01')],
-    ['OriginTz', 'Time'])
+# subscribe to a Kafka topic
+df = (
+    spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", "localhost:9092")
+    .option("subscribe", "topic1")
+    .load()
+)
 
-df.show()
+df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
